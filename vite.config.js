8d408e8fbf6b4ca17d13181fd31075ad33b14eeb
crosschainer <68580992+crosschainer@users.noreply.github.com>
2025-10-20 10:12:58 +0200

    Remove Vite image optimization plugin

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import viteImagemin from 'vite-plugin-imagemin';
 
 export default defineConfig({
   base: './',
-  plugins: [
-    react(),
-    viteImagemin({
-      gifsicle: {
-        optimizationLevel: 3,
-      },
-      mozjpeg: {
-        quality: 80,
-        progressive: true,
-      },
-      optipng: {
-        optimizationLevel: 5,
-      },
-      pngquant: {
-        quality: [0.7, 0.85],
-        speed: 3,
-      },
-      svgo: {
-        plugins: [
-          {
-            name: 'removeViewBox',
-            active: false,
-          },
-          {
-            name: 'removeEmptyAttrs',
-            active: false,
-          },
-        ],
-      },
-      webp: {
-        quality: 80,
-      },
-    }),
-  ],
+  plugins: [react()],
   build: {
     minify: 'esbuild',
     cssMinify: 'esbuild',
